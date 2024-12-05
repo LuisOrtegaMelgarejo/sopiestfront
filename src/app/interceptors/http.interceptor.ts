ac621e39dbf1b5ce1736a32dfc6c3accf4d655a4
LuisOrtegaMelgarejo <luis_ortega_m@hotmail.com>
2024-12-05 15:04:09 -0500

    se cambia a local

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -5,7 +5,7 @@ import {Observable} from 'rxjs';
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const apiReq = req.clone({ url: `http://127.0.0.1:8000/${req.url}`, headers: req.headers.set('rejectUnauthorized', 'false') });
+    const apiReq = req.clone({ url: `localhost:8000/${req.url}`, headers: req.headers.set('rejectUnauthorized', 'false') });
     return next.handle(apiReq);
   }
 }
