bcd98449889c0be1984037ea713117c0aa78245d
LuisOrtegaMelgarejo <luis_ortega_m@hotmail.com>
2024-12-05 12:36:50 -0500

    se agrega ssl

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -5,7 +5,7 @@ import {Observable} from 'rxjs';
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const apiReq = req.clone({ url: `http://167.172.13.72/${req.url}` });
+    const apiReq = req.clone({ url: `https://167.172.13.72/${req.url}`, headers: req.headers.set('rejectUnauthorized', 'false') });
     return next.handle(apiReq);
   }
 }
